3427c77bf6d23b05f5a2dca3e777626f5609ca0e
syafiqirz <118156554+syafiqirz@users.noreply.github.com>
2025-06-16 14:51:16 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -194,4 +194,5 @@ def make_prediction(gender, age_months, height_cm, weight_kg):
     }
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
