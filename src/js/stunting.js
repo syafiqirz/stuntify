cac67e14687b2d15db0603ae293b1456b9b28906
Syafiqirz <syafiqirz@Syafiqirzs-MacBook-Air.local>
2025-06-22 09:32:02 +0700

    deploy fe

--- src/js/stunting.js
+++
@@ -1,8 +0,0 @@
-// Stunting page entry point
-import '../css/stunting.css';
-import './modules/navbar-scroll.js';
-import './modules/custom-ai-assistant.js';
-import './modules/stunting-charts.js';
-import './modules/stunting-recommendations.js';
-
-console.log('Stunting page loaded with Webpack');
